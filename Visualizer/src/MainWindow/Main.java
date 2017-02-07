751e4873033d48bf2f587ce6ea9a006a2dae2da0
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-02-07 13:45:05 +0100

    SW-88: method to create a file with query in

--- Visualizer/src/MainWindow/Main.java
+++ Visualizer/src/MainWindow/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static parsers.UPPAALParser.generateQueryFile;
+
 public class Main extends Application {
 
     @Override
