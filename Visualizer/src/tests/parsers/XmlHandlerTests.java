52853dbb0e2d64ba21c7971bc40c8c3101679815
Kevin <kjarge12@student.aau.dk>
2017-02-07 10:39:09 +0100

    SW-90: test pakker/filer

---
+++ Visualizer/src/tests/parsers/XmlHandlerTests.java
@@ -0,0 +1,34 @@
+package tests.parsers;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Created by batto on 07-Feb-17.
+ */
+public class XmlHandlerTests {
+
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    @Test
+    void succeedingTest() {
+    }
+
+    @Test
+    void failingTest() {
+        //fail("a failing test");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
+}
