6361033f919dd017eb4179916f5139c6eee37802
Kevin <kjarge12@student.aau.dk>
2017-02-07 13:17:49 +0100

    SW-88: split query executer and visualizer

--- Visualizer/src/MainWindow/MainWindowController.java
+++ Visualizer/src/MainWindow/MainWindowController.java
@@ -1,17 +1,14 @@
 package MainWindow;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
@@ -27,7 +24,7 @@ public class MainWindowController implements Initializable {
 
     public void showExecutor(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("../UPPAALExecutor/UPPAALExecutor.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("../UPPAALExecutor/QueryVisualizer.fxml"));
             uppaalStage.setTitle("UPPAAL Executor");
             uppaalStage.setScene(new Scene(root, 300, 275));
             uppaalStage.show();
