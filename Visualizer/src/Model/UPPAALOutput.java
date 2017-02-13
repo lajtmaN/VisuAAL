90a8f75b6e80304780e6ca9396f4d1573db4009a
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-02-13 09:35:35 +0100

    SW-98: Can now parse uppaal error if something goes wrong

--- Visualizer/src/Model/UPPAALOutput.java
+++ Visualizer/src/Model/UPPAALOutput.java
@@ -4,4 +4,18 @@ package Model;
  * Created by lajtman on 07-02-2017.
  */
 public class UPPAALOutput {
+
+    private String errorDescription;
+
+    public String getErrorDescription() {
+        return errorDescription;
+    }
+
+    public void setErrorDescription(String errorDescription) {
+        this.errorDescription = errorDescription;
+    }
+
+    public Boolean errorState() {
+        return !(errorDescription == null || errorDescription.length() == 0);
+    }
 }
