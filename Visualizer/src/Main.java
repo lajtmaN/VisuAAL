56d509fd6067b4c10235d4363ede9849b956284b
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-02-13 12:35:04 +0100

    SW-107: Renamed TopologyViewer to MainWindow

--- Visualizer/src/Main.java
+++ Visualizer/src/Main.java
@@ -1,19 +1,15 @@
-import Helpers.ConnectedGraphGenerator;
 import javafx.application.Application;
-import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-        Parent root = FXMLLoader.load(getClass().getResource("View/TopologyViewer/TopologyViewer.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/MainWindow.fxml"));
         primaryStage.setTitle("UPPAAL Visualization Application");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
