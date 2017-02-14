e22885d525dd13f80336c026930ed232e0256de7
Kevin <kjarge12@student.aau.dk>
2017-02-14 15:21:04 +0100

    SW-113: input fra tabel ift template updates

---
+++ Visualizer/src/View/EditingCell.java
@@ -0,0 +1,44 @@
+package View;
+
+import Model.TemplateUpdate;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldTableCell;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Created by batto on 14-Feb-17.
+ */
+public class EditingCell<T, C> extends TextFieldTableCell<T, C> {
+    protected final TextField textField = new TextField();
+
+    public EditingCell() {
+        super();
+        textField.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+            if (! isNowFocused) {
+                processEdit();
+            }
+        });
+        textField.setOnAction(event -> processEdit());
+    }
+
+
+    protected void processEdit() { }
+
+    @Override
+    public void commitEdit(C value) {
+        super.commitEdit(value);
+    }
+
+    @Override
+    public void startEdit() {
+        super.startEdit();
+    }
+
+    @Override
+    public void cancelEdit() {
+        super.cancelEdit();
+    }
+}
