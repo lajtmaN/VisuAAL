b14e86931b744b68f152c969f854ee976503574f
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-02-14 13:36:22 +0100

    SW-110: adds serialization and stuff for saving simulations on generation

--- Visualizer/src/Model/UPPAALModel.java
+++ Visualizer/src/Model/UPPAALModel.java
@@ -9,6 +9,7 @@ import parsers.UPPAALParser;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.stream.Collectors;
@@ -16,10 +17,10 @@ import java.util.stream.Collectors;
 /**
  * Created by batto on 10-Feb-17.
  */
-public class UPPAALModel {
+public class UPPAALModel implements Serializable {
     private UPPAALTopology topology;
     private ArrayList<CVar<Integer>> constantVars;
-    private ObservableList<OutputVariable> outputVars = FXCollections.observableArrayList();
+    private transient ObservableList<OutputVariable> outputVars;
 
     private String uppaalPath;
 
@@ -30,6 +31,7 @@ public class UPPAALModel {
     public void load() {
         constantVars = UPPAALParser.getUPPAALConfigConstants(uppaalPath);
         topology = UPPAALParser.getUPPAALTopology(uppaalPath);
+        outputVars = FXCollections.observableArrayList();
         outputVars.setAll(UPPAALParser.getUPPAALOutputVars(uppaalPath, constantVars));
     }
 
@@ -50,6 +52,30 @@ public class UPPAALModel {
     }
 
     public Simulation runSimulation(String query) throws IOException {
-        return new Simulation(this, query);
+        //TODO we only use the first simulation
+        ArrayList<SimulationEdgePoint> points = UPPAALExecutor.provideQueryResult(getUppaalPath(), query)
+                .getZippedForSimulate(0);
+        return new Simulation(this, query, points);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UPPAALModel that = (UPPAALModel) o;
+
+        if (!topology.equals(that.topology)) return false;
+        if (!constantVars.equals(that.constantVars)) return false;
+        return uppaalPath.equals(that.uppaalPath);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = topology.hashCode();
+        result = 31 * result + constantVars.hashCode();
+        result = 31 * result + outputVars.hashCode();
+        result = 31 * result + uppaalPath.hashCode();
+        return result;
     }
 }
