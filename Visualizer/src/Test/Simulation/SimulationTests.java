a5f580506e0efed0f04201c947a832514b2a9a95
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-02-15 10:32:13 +0100

    SW-112: More robust serialization

--- Visualizer/src/Test/Simulation/SimulationTests.java
+++ Visualizer/src/Test/Simulation/SimulationTests.java
@@ -7,6 +7,10 @@ import Model.UPPAALModel;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
@@ -37,6 +41,39 @@ public class SimulationTests  {
         assertEquals(sim, sim2);
 
         File file = new File(FileHelper.simulationFileName(filePath));
+        assertTrue(file.delete());  
+
+    }
+
+    @Test
+    public void testSaveSimulationAndLoadSimulationOfDeletedModel() throws IOException {
+        String simulationFileName = Long.toHexString(Double.doubleToLongBits(Math.random()));
+        String uppaalModelFile = Long.toHexString(Double.doubleToLongBits(Math.random())) + ".xml";
+        Files.copy(new File("topologytest.xml").toPath(), new File(uppaalModelFile).toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        //Create simulation
+        UPPAALModel model = new UPPAALModel(uppaalModelFile);
+        model.load();
+        String query = "test";
+        ArrayList<SimulationEdgePoint> points = new ArrayList<>();
+        points.add(new SimulationEdgePoint(0.0, 0, 1, 0.0));
+        points.add(new SimulationEdgePoint(0.0, 1, 0, 0.0));
+        points.add(new SimulationEdgePoint(0.2, 0, 1, 1.0));
+        points.add(new SimulationEdgePoint(0.2, 1, 0, 1.0));
+        Simulation sim = new Simulation(model, query, points);
+
+        //Persist to file
+        sim.save(simulationFileName);
+
+        //Delete uppaal file tested on
+        assertTrue(new File(uppaalModelFile).delete());
+
+        //Load simulation again and check if still equal
+        Simulation sim2 = Simulation.load(simulationFileName);
+        assertEquals(sim, sim2);
+
+        //cleanup
+        File file = new File(FileHelper.simulationFileName(simulationFileName));
         assertTrue(file.delete());
 
     }
