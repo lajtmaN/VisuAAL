be369ec5fd9f27fc49c99a4b9dc3164c1a485795
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-02-15 11:38:11 +0100

    SW-112: Can make updates in XML files

--- Visualizer/src/Helpers/FileHelper.java
+++ Visualizer/src/Helpers/FileHelper.java
@@ -1,5 +1,8 @@
 package Helpers;
 
+import java.io.*;
+import java.nio.file.Files;
+
 /**
  * Created by lajtman on 14-02-2017.
  */
@@ -11,4 +14,20 @@ public class FileHelper {
     public static String getExtension(String filename) {
         return filename.substring(filename.lastIndexOf('.'));
     }
-}
+
+    public static File copyFileIntoTempFile(File original) throws IOException {
+        String content = "";
+        for (String s : Files.readAllLines(original.toPath())) {
+            content += s;
+        }
+        return generateAutoDeletedTempFile(content);
+    }
+    public static File generateAutoDeletedTempFile(String content) throws IOException {
+        File f = File.createTempFile("uppaalvisualizer_", ".tmp");
+        f.deleteOnExit();
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), "UTF-8"))) {
+            writer.write(content);
+        }
+        return f;
+    }
+ }
