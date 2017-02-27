dee5b52a796fa9d8e451136a6e4f63029ac261ce
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-02-27 10:47:32 +0100

    SW-137: Started working on DeclarationUpdater

---
+++ Visualizer/src/Test/parsers/Declaration/DeclarationUpdaterTests.java
@@ -0,0 +1,61 @@
+package parsers.Declaration;
+
+import java.io.*;
+import java.util.ArrayList;
+
+import Helpers.FileHelper;
+import Model.CVar;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+import parsers.CHandler;
+import parsers.Declaration.ANTLRGenerated.VariableParser;
+import parsers.UPPAALParser;
+import parsers.XmlHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by lajtman on 27-02-2017.
+ */
+public class DeclarationUpdaterTests {
+
+    @Test
+    public void doesNotAlterFileIfNoUpdates() throws IOException, ParserConfigurationException, SAXException {
+        File f = FileHelper.copyFileIntoTempFile(new File("mac_model_test.xml"));
+
+        XmlHandler handler = new XmlHandler(f.getPath());
+        String originalDecls = handler.getGlobalDeclarations();
+
+        String updatedDecls = VariableParser.updateAllDeclarations(originalDecls, new ArrayList<>());
+
+        assertEquals(originalDecls, updatedDecls);
+    }
+
+    @Test
+    public void canUpdateDeclarationsCorrectly() throws IOException, ParserConfigurationException, SAXException {
+        File f = FileHelper.copyFileIntoTempFile(new File("mac_model_test.xml"));
+        XmlHandler handler = new XmlHandler(f.getPath());
+        String orgDecls = handler.getGlobalDeclarations();
+
+        ArrayList<CVar<Integer>> cvars = CHandler.getConfigVariables(orgDecls, null);
+        assertNotNull(cvars);
+        //update some of them
+        cvars.get(0).setValue(12345);
+
+        String updatedDecls = VariableParser.updateAllDeclarations(orgDecls, cvars);
+        ArrayList<CVar<Integer>> newCvars = CHandler.getConfigVariables(updatedDecls, null);
+        assertNotNull(newCvars);
+
+        assertEquals(cvars.size(), newCvars.size());
+        for (int i = 0; i < cvars.size(); i++) {
+            assertEquals(cvars.get(i), newCvars.get(i));
+        }
+
+        //run new visitor
+        //parse the updated file and get config variables
+        //check if new config variables matches the updated ones
+    }
+}
--- Visualizer/src/parsers/Declaration/ANTLRGenerated/VariableParser.java
+++ Visualizer/src/parsers/Declaration/ANTLRGenerated/VariableParser.java
@@ -1,29 +1,51 @@
 package parsers.Declaration.ANTLRGenerated;
 
+import Model.CVar;
 import Model.UPPAALVariable;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import parsers.Declaration.ANTLRGenerated.uppaalParser.XtaContext;
+import parsers.Declaration.DeclarationUpdater;
 import parsers.Declaration.VariableInstantiationReader;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rasmu on 24/02/2017.
  */
 public class VariableParser {
-    private static XtaContext getRootElement(String declarations) {
-        ANTLRInputStream in = new ANTLRInputStream(declarations);
+    private static CommonTokenStream getTokenStream(String decls) {
+        ANTLRInputStream in = new ANTLRInputStream(decls);
         uppaalLexer lexer = new uppaalLexer(in);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        uppaalParser parser = new uppaalParser(tokens);
-        return parser.xta();
+        return new CommonTokenStream(lexer);
+    }
+
+    private static uppaalParser getParser(String declarations) {
+        return new uppaalParser(getTokenStream(declarations));
+    }
+
+    private static XtaContext getRootElement(String declarations) {
+        return getParser(declarations).xta();
     }
 
     public static ArrayList<UPPAALVariable> getInstantiations(String declaration) {
         VariableInstantiationReader listener = new VariableInstantiationReader();
         new ParseTreeWalker().walk(listener, getRootElement(declaration));
         return listener.getVariables();
+    }
+
+    /**
+     * Pretty prints the entire declaration and returns an updated source code with updated declarations
+     * @param updatedCVars
+     * @return
+     */
+    public static String updateAllDeclarations(String decl, List<CVar<Integer>> updatedCVars) {
+        CommonTokenStream tokens = getTokenStream(decl);
+        DeclarationUpdater updater = new DeclarationUpdater(tokens, updatedCVars);
+        ParseTreeWalker.DEFAULT.walk(updater, new uppaalParser(tokens).xta());
+        return updater.updatedDeclaration();
     }
 }
