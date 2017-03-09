a467a54ae7deb41c32a4cd3373be1cbc5e535fc4
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-03-09 11:38:37 +0100

    i: todo

--- Visualizer/src/View/Options/SimulationOption.java
+++ Visualizer/src/View/Options/SimulationOption.java
@@ -9,4 +9,6 @@ public abstract class SimulationOption {
     public abstract String getDescription();
 
     public abstract void performAction(Simulation currentSimulation);
+
+    //TODO maybe add icon
 }
