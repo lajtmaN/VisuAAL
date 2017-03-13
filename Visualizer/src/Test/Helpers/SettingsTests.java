e9b2142e5ede0952e5027e09a192d9d7de997307
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-03-13 14:10:19 +0100

    SW-145: Added settings object to store settings in and persist them

---
+++ Visualizer/src/Test/Helpers/SettingsTests.java
@@ -0,0 +1,31 @@
+package Helpers;
+
+import Model.Settings;
+import org.junit.Test;
+
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lajtman on 13-03-2017.
+ */
+public class SettingsTests {
+
+    @Test
+    public void canSaveAndLoadVerifytaLocation() throws IOException {
+        String verifytaLocation = "C:\\Program Files (x86)\\UPPAAL\\uppaal-4.1.19\\bin-Linux\\verifyta";
+
+        Settings settings = Settings.Instance();
+        settings.setVerifytaLocation(verifytaLocation);
+
+        String actualBeforeSave = settings.getVerifytaLocation();
+        assertEquals(verifytaLocation, actualBeforeSave);
+
+        settings.saveChanges();
+
+        String actualLoaded = settings.getVerifytaLocation();
+        assertEquals(verifytaLocation, actualLoaded);
+    }
+}
