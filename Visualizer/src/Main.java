5b766dd9aa23b005cad3d045b4d00f6aeb70309d
Kevin <kjarge12@student.aau.dk>
2017-03-13 11:55:32 +0100

    SW-149: enkelt transmitter template

--- Visualizer/src/Main.java
+++ Visualizer/src/Main.java
@@ -8,14 +8,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Get matrix for connected graph square topology. Input nr of nodes
+        //String s = Helpers.ConnectedGraphGenerator.optimizedMatrix(225);
+
         System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         Parent root = FXMLLoader.load(getClass().getResource("View/MainWindow.fxml"));
         primaryStage.setTitle("UPPAAL Visualization Application");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
-
-        //Get matrix for connected graph square topology. Input nr of nodes
-        //Helpers.ConnectedGraphGenerator.optimizedMatrix(25);
     }
 
     public static void main(String[] args) {
