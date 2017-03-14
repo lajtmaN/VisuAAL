c2069a2fce5389d66867cbe35b1d33fbd2892beb
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-03-14 14:27:49 +0100

    SW-160: Creates a lot show/hide options in simulation result viewer

---
+++ Visualizer/src/View/Options/ShowHideDataOption.java
@@ -0,0 +1,38 @@
+package View.Options;
+
+import Model.OutputVariable;
+import Model.Simulation;
+
+/**
+ * Created by lajtman on 14-03-2017.
+ */
+public class ShowHideDataOption extends EnableDisableSimulationOption {
+
+    private OutputVariable variable;
+
+    public ShowHideDataOption(OutputVariable var) {
+        this.variable = var;
+    }
+
+    @Override
+    public String getDescription() {
+        if (onProperty().get())
+            return "Show " + variable.getName();
+        else
+            return "Hide " + variable.getName();
+    }
+
+    @Override
+    public void startAction(Simulation currentSimulation) {
+        //TODO:
+        //currentSimulation.showDataFrom(variable);
+
+    }
+
+    @Override
+    public void disableAction(Simulation currentSimulation) {
+        //TODO:
+        // currentSimulation.hideDataFrom(variable);
+
+    }
+}
