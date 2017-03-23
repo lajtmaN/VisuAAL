5ebe356529eaa25dd8ff6262044516f036798e32
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-03-23 10:04:03 +0100

    SW-186: Get Google API Key from user

--- Visualizer/src/Main.java
+++ Visualizer/src/Main.java
@@ -1,3 +1,4 @@
+import Model.Settings;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,8 +14,13 @@ public class Main extends Application {
         //Get matrix for connected graph square topology. Input nr of nodes
         //String s = Helpers.ConnectedGraphGenerator.optimizedMatrix(196);
 
+        if (Settings.Instance().getGoogleAPIKey() == null) {
+            String key = JOptionPane.showInputDialog("Please specify your Google Developer API Key");
+            if (key == null || key.length() < 15)
+                System.exit(1);
 
-
+            Settings.Instance().setGoogleAPIKey(key);
+        }
 
         System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         Parent root = FXMLLoader.load(getClass().getResource("View/MainWindow.fxml"));
