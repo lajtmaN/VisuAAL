6024f24ce207d99d7624069bd9f47e271867d325
Kevin <kjarge12@student.aau.dk>
2017-04-03 14:52:04 +0200

    SW-211: Heat map data available

--- Visualizer/src/Model/Simulation.java
+++ Visualizer/src/Model/Simulation.java
@@ -13,12 +13,13 @@ import java.util.stream.Stream;
  */
 public class Simulation implements Serializable {
     private int currentSimulationIndex = 0;
-    private final List<? extends SimulationPoint> run;
+
+    protected final List<? extends SimulationPoint> simulationPoints;
     private Simulations parent;
     private boolean shown = false;
 
     public Simulation(List<? extends SimulationPoint> points) {
-        this.run = points;
+        this.simulationPoints = points;
     }
 
     public void initialize(Simulations parent, double modelTimeUnit) {
@@ -28,7 +29,7 @@ public class Simulation implements Serializable {
 
     private void applyModelTimeUnit(double modelTimeUnit) {
         if (modelTimeUnit != 1) {
-            for (SimulationPoint p : run) {
+            for (SimulationPoint p : simulationPoints) {
                 p.setClock(p.getClock() * modelTimeUnit);
             }
         }
@@ -38,14 +39,14 @@ public class Simulation implements Serializable {
                                   SimulationDataContainer varGridPane) {
         SimulationPoint sp;
         //Make sure that more elements at same end time all are included
-        while (!((sp = run.get(currentSimulationIndex)).getClock() > newTimeValue)) {
+        while (!((sp = simulationPoints.get(currentSimulationIndex)).getClock() > newTimeValue)) {
             if (sp.getClock() >= oldTime) {
                 boolean shown = pointIsShown(sp, newTimeValue) && sp.getValue() > 0;
                 parent.handleUpdate(sp, shown, sp.getValue(), globalVarGridPane, varGridPane);
             }
-            if (currentSimulationIndex + 1 >= run.size())
+            if (currentSimulationIndex + 1 >= simulationPoints.size())
                 break;
-            if (run.get(currentSimulationIndex + 1).getClock() <= newTimeValue)
+            if (simulationPoints.get(currentSimulationIndex + 1).getClock() <= newTimeValue)
                 currentSimulationIndex++;
             else break;
         }
@@ -54,14 +55,14 @@ public class Simulation implements Serializable {
     void markGraphBackwards(double newTimeValue, double oldTime, GridPane globalVarGridPane,
                                     SimulationDataContainer varGridPane) {
         SimulationPoint sp;
-        while (!((sp = run.get(currentSimulationIndex)).getClock() < newTimeValue)) {
+        while (!((sp = simulationPoints.get(currentSimulationIndex)).getClock() < newTimeValue)) {
             if (sp.getClock() <= oldTime) {
                 boolean shown = pointIsShown(sp, newTimeValue) && sp.getPreviousValue() > 0;
                 parent.handleUpdate(sp, shown, sp.getPreviousValue(), globalVarGridPane, varGridPane);
             }
             if (currentSimulationIndex - 1 < 0)
                 break;
-            if (run.get(currentSimulationIndex - 1).getClock() >= newTimeValue)
+            if (simulationPoints.get(currentSimulationIndex - 1).getClock() >= newTimeValue)
                 currentSimulationIndex--;
             else break;
         }
@@ -93,19 +94,19 @@ public class Simulation implements Serializable {
     }
 
     private Stream<? extends SimulationPoint> relatedSimulationPoints(OutputVariable variable) {
-        return run.stream().filter(data ->
+        return simulationPoints.stream().filter(data ->
                 data.getType() == variable.getCorrespondingSimulationPointType()
                         && data.getTrimmedIdentifier().equals(variable.getName()));
     }
 
     public void show() {
         shown = true;
-        handleUpdate(run);
+        handleUpdate(simulationPoints);
     }
 
     public void hide() {
         shown = false;
-        handleUpdate(run);
+        handleUpdate(simulationPoints);
     }
 
     public boolean isShown() {
@@ -120,13 +121,17 @@ public class Simulation implements Serializable {
         Simulation that = (Simulation) o;
 
         if (currentSimulationIndex != that.currentSimulationIndex) return false;
-        return run != null ? run.equals(that.run) : that.run == null;
+        return simulationPoints != null ? simulationPoints.equals(that.simulationPoints) : that.simulationPoints == null;
     }
 
     @Override
     public int hashCode() {
         int result = currentSimulationIndex;
-        result = 31 * result + (run != null ? run.hashCode() : 0);
+        result = 31 * result + (simulationPoints != null ? simulationPoints.hashCode() : 0);
         return result;
+    }
+
+    public List<? extends SimulationPoint> getSimulationPoints() {
+        return simulationPoints;
     }
 }
---
+++ Visualizer/src/Test/Simulation/HeatmapTests.java
@@ -0,0 +1,102 @@
+package Simulation;
+
+import Model.Heatmap;
+import Model.Simulation;
+import Model.SimulationPoint;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+/**
+ * Created by batto on 03-Apr-17.
+ */
+public class HeatmapTests {
+
+    @Test
+    public void TwoBoolSimulationsToHeatmap() {
+        ArrayList<SimulationPoint> points1 = new ArrayList<>(),
+                                   points2 = new ArrayList<>();
+
+        points1.add(new SimulationPoint("test1", 0, 1, 0));
+        points1.add(new SimulationPoint("test1", 1, 0, 1));
+        points1.add(new SimulationPoint("test1", 2, 1, 0));
+        points1.add(new SimulationPoint("test1", 3, 0, 1));
+        points1.add(new SimulationPoint("test1", 4, 1, 0));
+        points1.add(new SimulationPoint("test1", 5, 0, 1));
+        points1.add(new SimulationPoint("test1", 6, 1, 0));
+        points1.add(new SimulationPoint("test1", 7, 0, 1));
+
+        points2.add(new SimulationPoint("test1", 0, 1, 0));
+        points2.add(new SimulationPoint("test1", 2, 0, 1));
+        points2.add(new SimulationPoint("test1", 4, 1, 0));
+        points2.add(new SimulationPoint("test1", 7, 0, 1));
+
+        Simulation sim1 = new Simulation(points1),
+                   sim2 = new Simulation(points2);
+
+        ArrayList<Simulation> simulations = new ArrayList<>();
+        simulations.add(sim1);
+        simulations.add(sim2);
+
+        Heatmap heatmap = new Heatmap(simulations);
+
+        equalsData(0, 2, 0, heatmap.getDatapoint(0));
+        equalsData(1, 1, 2, heatmap.getDatapoint(1));
+        equalsData(2, 1, 1, heatmap.getDatapoint(2));
+        equalsData(3, 0, 1, heatmap.getDatapoint(3));
+        equalsData(4, 2, 0, heatmap.getDatapoint(4));
+        equalsData(5, 1, 2, heatmap.getDatapoint(5));
+        equalsData(6, 2, 1, heatmap.getDatapoint(6));
+        equalsData(7, 0, 2, heatmap.getDatapoint(7));
+    }
+
+    @Test
+    public void ThreeIntSimulationsToHeatmap() {
+        ArrayList<SimulationPoint> points1 = new ArrayList<>(),
+                points2 = new ArrayList<>(),
+                points3 = new ArrayList<>();
+
+        points1.add(new SimulationPoint("test2", 0, 8, 0));
+        points1.add(new SimulationPoint("test2", 1, 10, 8));
+        points1.add(new SimulationPoint("test2", 2, 2, 10));
+        points1.add(new SimulationPoint("test2", 5, -2, 2));
+        points1.add(new SimulationPoint("test2", 6, 10, -2));
+
+        points2.add(new SimulationPoint("test2", 0, 6, 0));
+        points2.add(new SimulationPoint("test2", 1, 4, 6));
+        points2.add(new SimulationPoint("test2", 5, 0, 4));
+        points2.add(new SimulationPoint("test2", 6, 3, 0));
+
+
+        points3.add(new SimulationPoint("test2", 0, 0, 0));
+        points3.add(new SimulationPoint("test2", 2, 5, 0));
+        points3.add(new SimulationPoint("test2", 5, -1, 5));
+        points3.add(new SimulationPoint("test2", 6, 15, -1));
+
+
+        Simulation sim1 = new Simulation(points1),
+                sim2 = new Simulation(points2),
+                sim3 = new Simulation(points3);
+
+        ArrayList<Simulation> simulations = new ArrayList<>();
+        simulations.add(sim1);
+        simulations.add(sim2);
+        simulations.add(sim3);
+
+        Heatmap heatmap = new Heatmap(simulations);
+
+        equalsData(0, 14, 0, heatmap.getDatapoint(0));
+        equalsData(1, 14, 14, heatmap.getDatapoint(1));
+        equalsData(2, 11, 14, heatmap.getDatapoint(2));
+        equalsData(5, -3, 11, heatmap.getDatapoint(3));
+        equalsData(6, 28, -3, heatmap.getDatapoint(4));
+    }
+
+
+    private void equalsData(int expectedTime, int expectedValue, int expectedPreviousValue, SimulationPoint point) {
+        assertEquals(expectedTime, point.getClock(), 0.1);
+        assertEquals(expectedValue, point.getValue(), 0.1);
+        assertEquals(expectedPreviousValue, point.getPreviousValue(), 0.1);
+    }
+}
