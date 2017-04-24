37f1825eddfa8c45d23ab5e1752a530a2360f4cf
Tim Hjermitslev Gjøderum <tgjade12@student.aau.dk>
2017-04-24 15:57:25 +0200

    SW-228: Delete references to GoogleMaps because we dont use it

---
+++ Visualizer/src/Model/topology/LatLng.java
@@ -0,0 +1,14 @@
+package Model.topology;
+
+/**
+ * Created by lajtman on 24-04-2017.
+ */
+public class LatLng {
+    public LatLng(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public double lat;
+    public double lng;
+}
