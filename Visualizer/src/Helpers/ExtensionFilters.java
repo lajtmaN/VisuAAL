0b96603483169bda2e34d0654bef315b45792bab
Rasmus <rpeter12@student.aau.dk>
2017-05-10 09:02:41 +0200

    i: changes extension filter

--- Visualizer/src/Helpers/ExtensionFilters.java
+++ Visualizer/src/Helpers/ExtensionFilters.java
@@ -10,7 +10,7 @@ public class ExtensionFilters {
     public static FileChooser.ExtensionFilter UPPAALModelExtensionFilter = new FileChooser.ExtensionFilter("UPPAAL Model", "*.xml");
     public static FileChooser.ExtensionFilter SimulationExtensionFilter = new FileChooser.ExtensionFilter("Simulation", "*.sim");
     public static FileChooser.ExtensionFilter TopologyExtensionFilter = new FileChooser.ExtensionFilter("Topology", "*.topo");
-    public static FileChooser.ExtensionFilter VerifytaExtensionFilter = new FileChooser.ExtensionFilter("Verifyta executable file", "verifyta.exe");
+    public static FileChooser.ExtensionFilter VerifytaExtensionFilter = new FileChooser.ExtensionFilter("Verifyta executable file", "verifyta*");
     public static FileChooser.ExtensionFilter GPSLogExtensionFilter = new FileChooser.ExtensionFilter("GPS Log", "*.txt");
     public static FileChooser.ExtensionFilter TopologySerializationFilter = new FileChooser.ExtensionFilter("Topology", "*.topology");
 }
