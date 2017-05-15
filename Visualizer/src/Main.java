9084250c49e7864402b3e779db3e4fd08d3bfe5e
Kevin <kjarge12@student.aau.dk>
2017-05-15 10:41:42 +0200

    SW-260: icon v1

--- Visualizer/src/Main.java
+++ Visualizer/src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -21,6 +22,7 @@ public class Main extends Application {
         primaryStage.setTitle("UPPAAL Visualization Application");
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("icon/icon.png")));
     }
 
     public static void main(String[] args) {
